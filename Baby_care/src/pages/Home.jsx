819b4e7e1a252d69070dc97d687f92909ef80a20
Hiruna Ilukpitiya <hirunalakshan00@gmail.com>
2024-05-07 21:07:09 +0530

    feat: Improve home page layout and content

--- Baby_care/src/pages/Home.jsx
+++ Baby_care/src/pages/Home.jsx
@@ -21,21 +21,23 @@ function Home() {
       <div className="relative">
         <div className="relative">
           <img className="homeBg w-full" src={homeBg} alt="Home_background" />
-          <div className=" absolute w-[340px] top-[200px] flex-row ">
+          <div className="flex justify-end pr-[5%]">
+          <div className=" absolute w-[40%] top-[200px] flex-row ">
             <h1 className="text-white font-extrabold font-inter flex items-center z-[1] text-3xl xl:text-6xl">
               We help people to get appointment in online
             </h1>
 
-            <p className=" text-black h-[151.3px] text-xl leading-[175%] font-light font-inter text-justify inline-block">
+            <p className=" text-white h-[151.3px] text-xl leading-[175%] font-light font-inter text-justify inline-block">
               Lorem Media is a full-service social media agency. We offer
               businesses innovative solutions that deliver the right type of
               audience to you in the most effective strategies possible. We
               strive to develop a community around your business, polishing your
               branding, and improving your public relations.
             </p>
-            <button onClick={NavToMidwifeLogin}>
+            <button onClick={NavToMidwifeLogin} className="flex justify-center">
               <img src= {midWlog} alt="midwife Login" className=" w-[318px] h-[67px]" />
             </button>
+          </div>
           </div>
         </div>
         <div className="w-full">
