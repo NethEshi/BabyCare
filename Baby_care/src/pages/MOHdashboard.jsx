0683a93f9e65fd8b0fcd57871a0e6ec4ebee4486
Eshani Kumarasinghe <eshanikumarasinghe@gmail.com>
2024-08-19 23:12:39 +0530

    create MOHSideBar, MOHDashNav, MOHLayout and update MOHsashboard

--- Baby_care/src/pages/MOHdashboard.jsx
+++ Baby_care/src/pages/MOHdashboard.jsx
@@ -1,9 +1,19 @@
-
+import MOHSideBar from "../components/MOH/MOHSideBar";
+import MOHDashNav from "../components/MOH/MOHDashNav";
 
 function MOHdashboard() {
   return (
-    <div>
-      <h1>MOH Dashboard</h1>
+    <div className="flex relative h-screen">
+      <div className="w-1/6">
+      <MOHSideBar />
+      </div>
+      <div className=" w-5/6 px-5 py-3">
+      <MOHDashNav />
+      </div>
+      <>
+        <hr/>
+      </>
+
     </div>
   );
 }
